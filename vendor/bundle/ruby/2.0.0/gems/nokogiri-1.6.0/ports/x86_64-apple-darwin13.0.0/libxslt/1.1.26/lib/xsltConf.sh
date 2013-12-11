751ed9f17fba8d2f33b5fdb8df112357de8209c3
umeyuki <umeyuki1326+github@gmail.com>
2013-12-11 22:18:54 +0900

    vendor gitignore

--- vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/umeyuki1326/Dropbox/dev/umeyuki/ruby-wikipedia-pornstars/vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/lib"
-XSLT_LIBS="-lxslt  -L/Users/umeyuki1326/Dropbox/dev/umeyuki/ruby-wikipedia-pornstars/vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
-XSLT_INCLUDEDIR="-I/Users/umeyuki1326/Dropbox/dev/umeyuki/ruby-wikipedia-pornstars/vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.0/ports/x86_64-apple-darwin13.0.0/libxslt/1.1.26/include"
-MODULE_VERSION="xslt-1.1.26"
