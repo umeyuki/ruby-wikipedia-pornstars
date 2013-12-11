b37e2b5623fa4b165d7a23c7cc3123988dab3372
umeyuki <umeyuki1326+github@gmail.com>
2013-12-11 22:27:43 +0900

    update schema

--- etc/initial_tables.sql
+++ etc/initial_tables.sql
@@ -2,8 +2,8 @@ drop table if exists porn_stars;
 create table porn_stars (
        name text,
        yomi text,
-       first_name text,
        last_name text,
+       first_name text,
        created_at text,
        updated_at text,
        deleted_at text
